97b20c40684c4f9f30ce6ee2a1ebc528aea0b965
Tom Chan <tomchanthree@gmail.com>
2017-09-07 01:17:50 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,15 @@
+def my_collect(array) # put argument(s) here
+  # code here
+  new_array = []
+  i = 0
+  while i < array.length
+    new_array << yield(array[i])
+    i = i + 1
+  end
+  new_array
+end
 
 
+my_collect(['ruby', 'javascript', 'python', 'objective-c']) do |language|
+  language.upcase
+end
